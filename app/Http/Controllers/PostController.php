5cfe1bdda02d58e12b5824ddeb560a832a25143c
Zac <a19860101@gmail.com>
2024-12-17 19:28:28 +0800

    20241217

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -31,6 +31,8 @@ class PostController extends Controller
     public function store(Request $request)
     {
         //
+
+        return $request;
     }
 
     /**
--- resources/views/post/create.blade.php
+++ resources/views/post/create.blade.php
@@ -12,7 +12,21 @@
         <a href="{{route('post.create')}}">新增文章</a>
     </nav>
     <div class="container">
-        <h1>新增文章</h1>
+        <div>
+            <form action="{{route('post.store')}}" method="post">
+                @csrf
+                <div>
+                    <label for="">文章標題</label>
+                    <input type="text" name="title">
+                </div>
+                <div>
+                    <label for="">內文</label>
+                    <textarea name="body" id="" cols="30" rows="10"></textarea>
+                </div>
+                <input type="submit" value="建立文章">
+                <input type="button" value="取消" onclick="history.back()">
+            </form>
+        </div>
     </div>
 </body>
 </html>

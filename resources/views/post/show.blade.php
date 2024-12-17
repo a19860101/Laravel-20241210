9f362dcb6c4a76dcfd95d593e1cbade15d5b04b5
Zac <a19860101@gmail.com>
2024-12-17 20:12:01 +0800

    20241217

--- resources/views/post/show.blade.php
+++ resources/views/post/show.blade.php
@@ -1,2 +1,12 @@
 <x-master>
+    <div class="container">
+        <h2>{{$post->title}}</h2>
+        <div>
+            最後更新時間:{{$post->updated_at}}
+        </div>
+        <div>
+            {{$post->body}}
+        </div>
+        <a href="{{route('post.index')}}">文章列表</a>
+    </div>
 </x-master>

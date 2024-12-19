de0293fbc5152691ed2ac3914495d32950e4b274
Zac <a19860101@gmail.com>
2024-12-19 19:30:33 +0800

    20241219

--- resources/views/components/master.blade.php
+++ resources/views/components/master.blade.php
@@ -5,10 +5,10 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Document</title>
-    <link rel="stylesheet" href="/styles/style.css">
+    @vite('resources/css/app.css')
 </head>
 <body>
-    <nav>
+    <nav class="p-4 bg-zinc-900 text-zinc-100">
         <a href="{{route('post.index')}}">首頁</a>
         <a href="{{route('post.create')}}">新增文章</a>
         <a href="#">聯絡我</a>
--- resources/views/post/index.blade.php
+++ resources/views/post/index.blade.php
@@ -1,14 +1,26 @@
 <x-master>
-    <div class="container">
-        <h1>文章列表</h1>
+    <div class="container flex mx-auto flex-wrap">
         @foreach($posts as $post)
-        <div>
-            <h2>{{$post->title}}</h2>
-            <div>最後更新時間:{{$post->updated_at}}</div>
-            <div>
-                {{Str::limit($post->body,100)}}
+        <div class="w-1/4 p-3">
+            <div class="border border-zinc-600">
+
+                <div class="aspect-square">
+                    {{-- <img src="/storage/{{$post->cover}}" alt=""> --}}
+                    @if($post->cover == null)
+                    <img src="https://placehold.co/600x400?text=No+Cover" alt="" class="w-full h-full object-cover">
+                    @else
+                    <img src="{{asset('storage/'.$post->cover)}}" alt="" class="w-full h-full object-cover">
+                    @endif
+                </div>
+                <div class="p-3">
+                    <h2>{{$post->title}}</h2>
+                    <div>最後更新時間:{{$post->updated_at}}</div>
+                    <div>
+                        {{Str::limit($post->body,100)}}
+                    </div>
+                    <a href="{{route('post.show',$post->id)}}" class="text-sm px-8 py-2 bg-amber-300 rounded">繼續閱讀</a>
+                </div>
             </div>
-            <a href="{{route('post.show',$post->id)}}">繼續閱讀</a>
         </div>
         @endforeach
     </div>

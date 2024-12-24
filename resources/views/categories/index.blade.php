c7d08a427c883d51395901635746b0ecfaed3f80
Zac <a19860101@gmail.com>
2024-12-24 20:05:25 +0800

    20241224

--- resources/views/categories/index.blade.php
+++ resources/views/categories/index.blade.php
@@ -1,14 +1,14 @@
 <x-master>
     <div class="container mx-auto mb-5">
         <h1 class="text-3xl font-bold">分類</h1>
-        <a href="{{route('category.create')}}" class="inline-block bg-blue-400 px-8 py-2">新增分類</a>
+        <a href="{{route('category.create')}}" class="rounded inline-block bg-blue-400 px-8 py-2">新增分類</a>
 
     </div>
     <div class="container mx-auto">
         <table class="w-96 border border-zinc-500">
             @foreach($categories as $category)
             <tr class=" border border-zinc-500">
-                <td>{{$category->title}}</td>
+                <td class="p-2">{{$category->title}}</td>
             </tr>
             @endforeach
         </table>
